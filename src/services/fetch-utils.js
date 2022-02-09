8d5a39b7a1288b0d3687f56b6e65c4ff5ad4768f
Philippe Ngom <philngom@gmail.com>
2022-02-09 11:13:23 -0800

    created fetchSinglePokemon in fetch-utils.js

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -7,3 +7,13 @@ export async function fetchPokemons(start = 1, end = 40) {
     .range(start, end);
   return checkError(data);
 }
+
+export async function fetchSinglePokemon(id) {
+  const data = await client
+    .from('pokemon')
+    .select()
+    .match({ id: id })
+    .single();
+
+  return checkError(data);
+}
