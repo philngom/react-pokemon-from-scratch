d9929c3a472815ea6f46f4c2e75d1f8c68b2ccbd
Philippe Ngom <philngom@gmail.com>
2022-02-09 12:21:15 -0800

    testing CI

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/current page/i);
+  const linkElement = screen.getByText(/current/i);
   expect(linkElement).toBeInTheDocument();
 });
