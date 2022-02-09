bd4cdb085305fc0dbd340e6b88ccd58c94ef03a5
Philippe Ngom <philngom@gmail.com>
2022-02-09 09:52:08 -0800

    finished PokemonList, created Pokemon component, passed Pokemon pokemon prop from parent PokemonList

---
+++ src/PokemonList.js
@@ -0,0 +1,14 @@
+import Pokemon from './Pokemon';
+
+export default function PokemonList({ pokemons }) {
+
+  return (
+    <div className='pokemons-container'>
+      {
+        pokemons.map((pokemon, i) =>
+          <Pokemon key={pokemon + i} pokemon={pokemon}/>
+        )
+      }
+    </div>
+  );
+}
