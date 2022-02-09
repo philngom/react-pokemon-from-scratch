bdc7e7ad388e30b3190d94a64f7783def85a1400
Philippe Ngom <philngom@gmail.com>
2022-02-09 08:17:22 -0800

    completed useEffect function on PokemonPage

--- src/PokemonPage.js
+++ src/PokemonPage.js
@@ -1,13 +1,21 @@
 import { fetchPokemons } from '../services/fetch-utils.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
 
+  const pokemonsPerPage = 40;
+  const [page, setPage] = useState(1);
+  const [pokemons, setPokemons] = useState([]);
+
   useEffect(() => {
     async function fetch() {
-
+      const start = page * pokemonsPerPage - pokemonsPerPage;
+      const end = page * pokemonsPerPage;
+      const data = await fetchPokemons(start, end);
+      setPokemons(data);
     }
-  }, []);
+    fetch();
+  }, [page]);
 
   return (
     <div>
