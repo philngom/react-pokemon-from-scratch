bdccd3119d5ca1588ab24800fdb524fe9b2147ee
Philippe Ngom <philngom@gmail.com>
2022-02-09 12:13:15 -0800

    styling on detail complete

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,10 +1,39 @@
-import { Link } from 'react-router-dom';
+import './PokemonDetail.css';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { fetchSinglePokemon } from './services/fetch-utils.js';
 
-export default function PokemonDetail(id) {
+
+export default function PokemonDetail() {
+
+  const [pokemon, setPokemon] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const data = await fetchSinglePokemon(params.id);
+      setPokemon(data);
+    }
+    fetchPokemon();
+  }, [params.id]);
   return (
-    <div>
+    <div className="page">
       <Link to='/'>Home</Link>
-      you in here!
+      <div className="pokemon-detail">
+        <div className='info'>
+          <p>Name: {pokemon.pokemon}</p>
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+          <p>Ability: {pokemon.ability_1}</p>
+          <p>Attack: {pokemon.attack}</p>
+          <p>Defense: {pokemon.defense}</p>
+          <p>HP: {pokemon.hp}</p>
+
+        </div>
+        <div>
+          <img src={`${pokemon.url_image}`}/>
+        </div>
+      </div>
     </div>
   );
 }
