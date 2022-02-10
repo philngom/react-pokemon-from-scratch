f294a2888a3f8d467ac33889fc35b2922959c607
Philippe Ngom <philngom@gmail.com>
2022-02-09 16:48:15 -0800

    switched test method to findByText()

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/current/i);
+  const linkElement = screen.findByText(/current/i);
   expect(linkElement).toBeInTheDocument();
 });
